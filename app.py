b265cd84445671033cb778153cb382757d213b43
aktashaluk <78588425+aktashaluk@users.noreply.github.com>
2026-06-17 14:38:43 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ def convert(decimal_num):
 def main_post():
     if request.method == 'POST':
         alpha = request.form['number']
-        if not alpha.isdecimal():
-            return render_template('index.html', developer_name='Guile', not_valid=True)
+        if not alpha.isdigit():
+            return render_template('index.html', developer_name='Haluk', not_valid=True)
         number = int(alpha)
         if not 0 < number < 4000:
-            return render_template('index.html', developer_name='Guile', not_valid=True)
-        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Guile')
+            return render_template('index.html', developer_name='Haluk', not_valid=True)
+        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Haluk')
     else:
-        return render_template('index.html', developer_name='Guile', not_valid=False)
+        return render_template('index.html', developer_name='Haluk', not_valid=False)
 
 if __name__ == '__main__':
     # app.run(debug=True)
